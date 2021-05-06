45cb668205309a60f56fdb238d19b04195809bdd
jimmy123A <75439939+jimmy123A@users.noreply.github.com>
2021-05-06 19:35:35 +0300

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -3,12 +3,7 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyAA5aFQbiFHhKXZEJTZiMcvtbtuUGD-F7w",
-    authDomain: "netflix-867b0.firebaseapp.com",
-    projectId: "netflix-867b0",
-    storageBucket: "netflix-867b0.appspot.com",
-    messagingSenderId: "1044571613511",
-    appId: "1:1044571613511:web:10e8c58008d2439bf84462"
+   /* your firebase config */
 };
 
 const firebaseApp = firebase.initializeApp(config);
